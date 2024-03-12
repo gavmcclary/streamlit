ceda65a12e4a0c4b03a79888f699766450533501
gavmcclary <gavin.mcclary@gmail.com>
2024-03-12 15:31:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from openai import OpenAI
 
 try:
     client = OpenAI(api_key=st.text_input('Enter OpenAI API token:', type='password'),)
-    st.success('Proceed to entering your prompt message!', icon='👉')
 except Exception as e:
     st.text(e)
