a9699c26aff530ebc58802eb72e88b719a60a0e4
gavmcclary <gavin.mcclary@gmail.com>
2024-03-12 13:35:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import streamlit as st
-import openai
+from openai import OpenAI
+
+client = OpenAI()
 
 with st.sidebar:
     st.title('🤖💬 OpenAI Chatbot')
     if 'OPENAI_API_KEY' in st.secrets:
         st.success('API key already provided!', icon='✅')
-        openai.api_key = st.secrets['OPENAI_API_KEY']
+        OpenAI.api_key = st.secrets['OPENAI_API_KEY']
     else:
-        openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
+        OpenAI.api_key = st.text_input('Enter OpenAI API token:', type='password')
         if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
