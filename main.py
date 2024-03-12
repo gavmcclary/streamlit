7db74fcd0f54b34c9547298c04c2760aeee94e8a
gavmcclary <gavin.mcclary@gmail.com>
2024-03-12 15:33:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import streamlit as st
 from openai import OpenAI
 
-try:
-    client = OpenAI(api_key=st.text_input('Enter OpenAI API token:', type='password'),)
-except Exception as e:
-    st.text(e)
+st.text("Hello")
